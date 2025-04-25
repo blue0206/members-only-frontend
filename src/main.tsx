7aa622a1419da85a3e1a987d3bd95d0c03479791
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-25 21:56:13 +0530

    feat: initialize Sentry for production mode
    - Initialized Sentry for production environment with tracing, breadcrumbs, and replay integrations.

--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,31 @@ import { store } from "./app/store.ts";
 import * as Sentry from "@sentry/react";
 import { Button } from "./components/ui/button.tsx";
 
+// Sentry initialization for production.
+if (import.meta.env.PROD) {
+  Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    integrations: [
+      Sentry.browserTracingIntegration(),
+      Sentry.breadcrumbsIntegration(),
+      Sentry.replayIntegration(),
+    ],
+    tracesSampleRate: 0.8,
+    tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+    environment: import.meta.env.MODE,
+  });
+  console.log("Sentry initialized for production.");
+} else {
+  console.log("Sentry not initialized for development.");
+}
+
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
+      {/* Sentry Error Boundary wrapper for App. */}
       <Sentry.ErrorBoundary
         fallback={
           <div>
