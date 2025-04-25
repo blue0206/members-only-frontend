9bc46c40036655193ad6f5a048a2f5ffc7c29630
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-25 13:32:06 +0530

    feat: concat apiSlice middleware in store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,14 @@
 import authSlice from "@/features/auth/authSlice";
 import { configureStore } from "@reduxjs/toolkit";
-import { apiSlice } from "./api";
+import { apiSlice } from "./services/api";
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authSlice,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
 export type AppStore = typeof store;
