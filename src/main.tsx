86dc908142df7d10f0e09c6231dc448dbb101444
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-25 21:45:22 +0530

    feat: add sentry error boundary
    - Added Sentry error boundary to the main application entry point.
    - Include a proper fallback message and a reload button for the error.

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,30 @@ import "./index.css";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { store } from "./app/store.ts";
+import * as Sentry from "@sentry/react";
+import { Button } from "./components/ui/button.tsx";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <Sentry.ErrorBoundary
+        fallback={
+          <div>
+            <h2>Oops! Something went wrong.</h2>
+            <Button
+              onClick={() => {
+                window.location.reload();
+              }}
+            >
+              Reload Page
+            </Button>
+          </div>
+        }
+        showDialog={import.meta.env.DEV}
+      >
+        <App />
+      </Sentry.ErrorBoundary>
     </Provider>
   </StrictMode>
 );
