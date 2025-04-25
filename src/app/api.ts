26118c08fd28cfc008705d8a24a4309f09583d9a
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-25 13:09:18 +0530

    feat: implement registerUser mutation in apiSlice

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,3 +1,8 @@
+import {
+  ApiResponse,
+  RegisterRequestDto,
+  RegisterResponseDto,
+} from "@blue0206/members-only-shared-types";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
@@ -7,5 +12,20 @@ const baseQuery = fetchBaseQuery({
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery,
-  endpoints: (builder) => ({}),
+  endpoints: (builder) => ({
+    registerUser: builder.mutation<RegisterResponseDto, RegisterRequestDto>({
+      query: (body: RegisterRequestDto) => ({
+        url: "/auth/register",
+        method: "POST",
+        body,
+      }),
+      transformResponse: (returnValue: ApiResponse<RegisterResponseDto>) => {
+        if (returnValue.success) {
+          return returnValue.data;
+        } else {
+          throw new Error(returnValue.error.message);
+        }
+      },
+    }),
+  }),
 });
