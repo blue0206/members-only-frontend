db38990965bb1b219ebad4382735a57c83c99192
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-25 13:10:49 +0530

    feat: add apiSlice reducer to store configuration

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,10 @@
 import authSlice from "@/features/auth/authSlice";
 import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./api";
 
 export const store = configureStore({
   reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authSlice,
   },
 });
