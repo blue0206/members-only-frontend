412a20f17faf4e89bf76f2429a021bfb726b9f34
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-25 13:44:31 +0530

    feat: add login mutation to authApi
    This commit introduces the `loginUser` mutation to the `authApiSlice`.
    This mutation allows users to log in to the application.
    
    The following changes were made:
    
    - Added `loginUser` mutation to `authApiSlice` in `src/app/services/authApi.ts`.

--- src/app/services/authApi.ts
+++ src/app/services/authApi.ts
@@ -1,5 +1,7 @@
 import {
   ApiResponse,
+  LoginRequestDto,
+  LoginResponseDto,
   RegisterRequestDto,
   RegisterResponseDto,
 } from "@blue0206/members-only-shared-types";
@@ -14,6 +16,20 @@ export const authApiSlice = apiSlice.injectEndpoints({
         body,
       }),
       transformResponse: (returnValue: ApiResponse<RegisterResponseDto>) => {
+        if (returnValue.success) {
+          return returnValue.data;
+        } else {
+          throw new Error(returnValue.error.message);
+        }
+      },
+    }),
+    loginUser: builder.mutation<LoginResponseDto, LoginRequestDto>({
+      query: (body: LoginRequestDto) => ({
+        url: "/auth/login",
+        method: "POST",
+        body,
+      }),
+      transformResponse: (returnValue: ApiResponse<LoginResponseDto>) => {
         if (returnValue.success) {
           return returnValue.data;
         } else {
