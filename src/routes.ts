643354374aaf7be012635e0ddc4aba72d7bc1984
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-26 14:16:51 +0530

    feat: add route for Error component to handle error pages

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router";
 import Home from "./features/home/Home";
+import Error from "./components/shared/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     Component: Home,
+  },
+  {
+    path: "/error",
+    Component: Error,
   },
 ]);
 
