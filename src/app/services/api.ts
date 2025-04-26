cb0bb1b9eeb33e882c419d7ee8586c6aac9fb435
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-26 17:39:08 +0530

    feat: add CSRF token and access token to API requests
    This commit introduces the following changes:
    
    - Adds CSRF token to the headers of API requests for endpoints that require it.
    - Adds access token to the headers of API requests for endpoints that require it.
    - Updates the `apiSlice` to include the necessary logic for adding these tokens.
    - Updates the `apiSlice` to include the `logoutUser` and `refreshTokens` endpoints in the list of endpoints that require CSRF and access tokens.
    
    These changes improve the security of the application by ensuring that
    all API requests are properly authenticated.

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,7 +1,41 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: import.meta.env.VITE_API_BASE_URL,
+  prepareHeaders: (headers, { getState, endpoint }) => {
+    // Define endpoints where both access token and CSRF token are required.
+    const commonEndpoints = ["logoutUser"];
+    // Define endpoints where only CSRF token is required.
+    const csrfEndpoints = [...commonEndpoints, "refreshTokens"];
+    // Define endpoints where only access token is required.
+    const accessTokenEndpoints = [...commonEndpoints];
+
+    // Add access token to headers if the current endpoint
+    // is listed in access token endpoints.
+    if (accessTokenEndpoints.includes(endpoint)) {
+      // Get access token from store.
+      const accessToken = (getState() as RootState).auth.accessToken;
+      // Append to headers if it is present.
+      if (accessToken) headers.append("Authorization", `Bearer ${accessToken}`);
+    }
+    // Add CSRF token to headers if the current endpoint is listed in
+    // CSRF endpoints.
+    if (csrfEndpoints.includes(endpoint)) {
+      // Get all cookies in array.
+      const cookies = document.cookie.split(";");
+      // Get CSRF cookie from array.
+      const csrfCookie = cookies.find((cookie) =>
+        cookie.startsWith("csrf-token")
+      );
+      // Extract CSRF token from cookie.
+      const csrfToken = csrfCookie?.split("=")[1];
+      // If present, populate header.
+      if (csrfToken) headers.append("x-csrf-token", csrfToken.trim());
+    }
+
+    return headers;
+  },
 });
 
 export const apiSlice = createApi({
