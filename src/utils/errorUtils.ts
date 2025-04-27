89932ec4d9d80f43f950af20b3796222e8e4c12e
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-27 14:45:17 +0530

    feat: add isApiResponseError type guard for API response errors

---
+++ src/utils/errorUtils.ts
@@ -0,0 +1,15 @@
+import { ApiResponseError } from "@blue0206/members-only-shared-types";
+
+export const isApiResponseError = (
+  error: unknown
+): error is ApiResponseError => {
+  if (
+    typeof error === "object" &&
+    error &&
+    "success" in error &&
+    "error" in error
+  ) {
+    return true;
+  }
+  return false;
+};
