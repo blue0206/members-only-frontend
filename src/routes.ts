b641b126803cbc55789cbd87525a80fa47d34e8d
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-30 19:09:33 +0530

    feat: add routes for Login and Register components

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,20 @@
 import { createBrowserRouter } from "react-router";
 import Home from "./features/home/Home";
 import Error from "./components/shared/Error";
+import { Login, Register } from "./features/auth";
 
 const router = createBrowserRouter([
   {
     path: "/",
     Component: Home,
+  },
+  {
+    path: "/login",
+    Component: Login,
+  },
+  {
+    path: "/register",
+    Component: Register,
   },
   {
     path: "/error",
