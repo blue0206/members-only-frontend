6eb22f76af43615a571a1608b586c0ee2a6f28b7
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-05-01 14:45:12 +0530

    feat: add actionHasSerializedError type guard
    Introduces the `actionHasSerializedError` type guard.
    This utility checks if a Redux action's payload is
    an RTK Query `SerializedError`.
    
    It differentiates these errors from API-originated errors
    (`ApiErrorPayload`) by verifying the presence of a `code` property that
    does not match any known `ApiErrorCode`.
    This is useful for handling errors potentially thrown during client-side
    processing within RTK Query, such as in `transformResponse`.

--- src/app/middlewares/typeGuards.ts
+++ src/app/middlewares/typeGuards.ts
@@ -1,4 +1,8 @@
-import { PayloadAction, UnknownAction } from "@reduxjs/toolkit";
+import {
+  PayloadAction,
+  SerializedError,
+  UnknownAction,
+} from "@reduxjs/toolkit";
 import { ApiErrorPayload } from "@blue0206/members-only-shared-types";
 import { isApiErrorCode } from "@/utils/errorUtils";
 
@@ -13,6 +17,23 @@ export const actionHasApiErrorPayload = (
     action.payload &&
     "code" in action.payload &&
     isApiErrorCode(action.payload.code)
+  ) {
+    return true;
+  }
+  return false;
+};
+
+// Type guard to check if the error payload of action is
+// from RTK Query SerializedError (errors thrown in transformResponse for example).
+export const actionHasSerializedError = (
+  action: UnknownAction
+): action is PayloadAction<SerializedError> => {
+  if (
+    "payload" in action &&
+    typeof action.payload === "object" &&
+    action.payload &&
+    "code" in action.payload &&
+    !isApiErrorCode(action.payload.code)
   ) {
     return true;
   }
