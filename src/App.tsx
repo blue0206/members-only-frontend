5e3e97b9e2bc7cb0f9e7619f5a69726610fb1e5a
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-05-01 16:19:22 +0530

    feat: update router import path and add Toaster component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { RouterProvider } from "react-router";
-import router from "./routes";
+import router from "@/routes";
+import { Toaster } from "./components/ui/sonner";
 
 function App() {
   return (
     <>
       <RouterProvider router={router} />
+      <Toaster richColors position="bottom-right" />
     </>
   );
 }
