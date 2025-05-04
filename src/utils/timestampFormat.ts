ca810fd55e71ae39f5d0baaaa7ac911e4df1599a
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-05-04 11:35:35 +0530

    feat: add getReadableDate utility for formatted date output

--- src/utils/timestampFormat.ts
+++ src/utils/timestampFormat.ts
@@ -1,5 +1,11 @@
 import { format } from "date-fns";
 
 export const getReadableTime = (timestamp: string): string => {
+  // 12:00 AM
   return format(timestamp, "p");
+};
+
+export const getReadableDate = (timestamp: string): string => {
+  // Apr 29, 1456
+  return format(timestamp, "PP");
 };
