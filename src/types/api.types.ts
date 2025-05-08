de3ff170ecbad1d9d89cd15ec2aa2bdc9e907f90
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-05-08 18:55:08 +0530

    feat: define query type for deleteUser endpoint
    Adds the `DeleteUserEndpointQueryType` interface. This type defines
    the structure for the query arguments of the `deleteUser` mutation,
    allowing for an optional `username`. This distinction is used to
    determine if a user is deleting their own account or if an
    administrator is deleting another user's account.

--- src/types/api.types.ts
+++ src/types/api.types.ts
@@ -1,10 +1,16 @@
 import {
   EditMessageRequestDto,
   MessageParamsDto,
+  UsernameParamsDto,
 } from "@blue0206/members-only-shared-types";
 
 // Export "editMessage" endpoint query type.
 export interface EditMessageEndpointQueryType {
   newMessage: EditMessageRequestDto["newMessage"];
   messageId: MessageParamsDto["messageId"];
+}
+
+// Export "deleteUser" endpoint query type.
+export interface DeleteUserEndpointQueryType {
+  username: UsernameParamsDto["username"] | null;
 }
