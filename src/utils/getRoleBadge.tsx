f54765973d939a3df1fd1282c5c6805cb5b83c11
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-06-04 19:09:38 +0530

    style: refine role badge appearance

--- src/utils/getRoleBadge.tsx
+++ src/utils/getRoleBadge.tsx
@@ -7,22 +7,31 @@ export function getRoleBadge(role: Role): React.ReactNode {
   switch (role) {
     case Role.USER:
       return (
-        <Badge className="w-fit flex items-center gap-1 bg-gray-100 text-gray-800 border-gray-200 dark:bg-gray-800 dark:text-gray-100">
-          <User className="h-4 w-4" />
+        <Badge
+          variant={"outline"}
+          className="rounded-xl flex text-xs items-center gap-1 bg-gray-100 text-gray-800 border-gray-200 dark:bg-gray-800 dark:text-gray-100"
+        >
+          <User className="h-3.5 w-3.5" />
           {role}
         </Badge>
       );
     case Role.MEMBER:
       return (
-        <Badge className="w-fit flex items-center gap-1 bg-blue-100 text-blue-800 border-blue-200 dark:bg-blue-800 dark:text-blue-100">
-          <Crown className="h-4 w-4" />
+        <Badge
+          variant={"outline"}
+          className="rounded-xl text-xs flex items-center gap-1 bg-blue-100 text-blue-800 border-blue-200 dark:bg-blue-800 dark:text-blue-100"
+        >
+          <Crown className="h-3.5 w-3.5" />
           {role}
         </Badge>
       );
     case Role.ADMIN:
       return (
-        <Badge className="w-fit flex items-center gap-1 bg-red-100 text-red-800 border-red-200 dark:bg-red-800 dark:text-red-100">
-          <Shield className="h-4 w-4" />
+        <Badge
+          variant={"outline"}
+          className="rounded-xl text-xs flex items-center gap-1 bg-red-100 text-red-800 border-red-200 dark:bg-red-800 dark:text-red-100"
+        >
+          <Shield className="h-3.5 w-3.5" />
           {role}
         </Badge>
       );
