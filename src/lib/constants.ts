3467dacbb1241e228f76636b586a7aa0fec33fbf
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-06-09 22:56:43 +0530

    feat: add session expired query constant
    This commit introduces a new constant `sessionExpiredQuery`
    to be used for identifying session expiration scenarios,
    via URL query parameters.

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,3 +5,5 @@ export const SortOptions = {
   mostBookmarks: "most-bookmarks",
 } as const;
 export type SortOptionsType = (typeof SortOptions)[keyof typeof SortOptions];
+
+export const sessionExpiredQuery = "SESSION_EXPIRED";
