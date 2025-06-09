056e34e59d8344bc0eacaa0a9e6b4f4f695209df
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-06-09 23:26:02 +0530

    feat: make toast notification position responsive

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,22 @@ import router from "@/routes";
 import { Toaster } from "./components/ui/sonner";
 import { useAppSelector } from "./app/hooks";
 import { getTheme } from "./features/ui/uiSlice";
+import { useMediaQuery } from "react-responsive";
 
 function App() {
+  const isDesktop = useMediaQuery({
+    query: "(min-width: 768px)",
+  });
   const currentTheme = useAppSelector(getTheme);
 
   return (
     <>
       <RouterProvider router={router} />
-      <Toaster richColors position="bottom-right" theme={currentTheme} />
+      <Toaster
+        richColors
+        position={isDesktop ? "bottom-right" : "top-center"}
+        theme={currentTheme}
+      />
     </>
   );
 }
