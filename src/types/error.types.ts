32dc450b68036a2d78b1784c84f8a26ef0bd1349
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-06-13 12:56:11 +0530

    refactor: move ErrorPageDetailsType to route.types
    The `ErrorPageDetailsType`, which is used for defining the state
    passed during navigation to the error page, has been moved from
    `src/types/error.types.ts` to a new file `src/types/route.types.ts`.
    
    This relocation better reflects its usage in the context of routing
    and navigation state.

--- src/types/error.types.ts
+++ src/types/error.types.ts
@@ -1,11 +1,6 @@
 import { ApiErrorPayload } from "@blue0206/members-only-shared-types";
 import { FetchBaseQueryError } from "@reduxjs/toolkit/query";
 
-export interface ErrorPageDetailsType {
-  statusCode: number;
-  message: string;
-}
-
 // Define custom base query error type which is to be returned.
 // It can either be from API or from RTK Query.
 export type CustomBaseQueryError = FetchBaseQueryError | ApiErrorPayload;
