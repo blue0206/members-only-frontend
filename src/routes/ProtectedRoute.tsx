2f29d9a4c8994f94e0b548a80b14879bd2af34d7
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-06-13 13:09:32 +0530

    feat: add ProtectedRoute component for RBAC
    Introduces a new `ProtectedRoute` component responsible for:
    - Checking if a user is authenticated. If not, redirects to the
      login page with the original intended location and a reason.
    - If authenticated, checks if the user's role is included in the
      `allowedRoles` prop (if provided).
    - If the role check fails, redirects the user to a generic error
      page with a 403 (Forbidden) status and an authorization message.
    - If all checks pass, renders the child routes using <Outlet />.
    
    This component will be used to guard routes that require
    authentication and/or specific user roles.

---
+++ src/routes/ProtectedRoute.tsx
@@ -0,0 +1,47 @@
+import { useAppSelector } from "@/app/hooks";
+import { getUserRole, isAuthenticated } from "@/features/auth/authSlice";
+import { unauthorizedRedirectionQuery } from "@/lib/constants";
+import { ErrorPageDetailsType } from "@/types";
+import { UnauthorizedRedirectionStateType } from "@/types/route.types";
+import { Role } from "@blue0206/members-only-shared-types";
+import { Navigate, Outlet, useLocation } from "react-router";
+
+interface ProtectedRoutePropsType {
+  allowedRoles?: Role[];
+}
+
+export default function ProtectedRoute({
+  allowedRoles,
+}: ProtectedRoutePropsType) {
+  const isAuth = useAppSelector(isAuthenticated);
+  const role = useAppSelector(getUserRole);
+  const location = useLocation();
+
+  if (!isAuth) {
+    return (
+      <Navigate
+        to={`/login?reason=${unauthorizedRedirectionQuery}`}
+        state={{ from: location } satisfies UnauthorizedRedirectionStateType}
+        replace
+      />
+    );
+  }
+
+  if (allowedRoles && role && !allowedRoles.includes(role)) {
+    // Redirect to Forbidden error page and provide a message to display.
+    return (
+      <Navigate
+        to="/error"
+        state={
+          {
+            message: "You are not authorized to access this page.",
+            statusCode: 403,
+          } satisfies ErrorPageDetailsType
+        }
+        replace
+      />
+    );
+  }
+
+  return <Outlet />;
+}
