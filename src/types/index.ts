14339ec0698d1c5b045e9cfaf49af1c7c54acffd
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-06-13 12:58:33 +0530

    refactor: update type exports in index.ts for better organization

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,8 @@
 export { HttpMethod } from "./http.types";
-export type { ErrorPageDetailsType, CustomBaseQueryError } from "./error.types";
+export type { CustomBaseQueryError } from "./error.types";
+export type { ErrorPageDetailsType } from "./route.types";
+export type {
+  DeleteUserEndpointQueryType,
+  EditMessageEndpointQueryType,
+  SetRoleEndpointQueryType,
+} from "./api.types";
