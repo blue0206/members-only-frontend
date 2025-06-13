206a8cf9a782d888aaa4260afc313764ca27a873
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-06-13 13:05:54 +0530

    feat: add UnauthorizedRedirectionStateType interface for unauth redirection

--- src/types/route.types.ts
+++ src/types/route.types.ts
@@ -1,4 +1,10 @@
+import type { Location } from "react-router";
+
 export interface ErrorPageDetailsType {
   statusCode: number;
   message: string;
+}
+
+export interface UnauthorizedRedirectionStateType {
+  from: Location;
 }
