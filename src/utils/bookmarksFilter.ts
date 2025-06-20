87dac6d7aa69c01f95044c08e523166f444d634a
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-06-21 00:26:01 +0530

    feat: implement search filter utility
    Introduce a new utility function, `bookmarksFilter`, to provide search
    functionality on the bookmarks page.
    
    This function filters an array of bookmarked messages based on a given
    search query.

---
+++ src/utils/bookmarksFilter.ts
@@ -0,0 +1,30 @@
+import { GetUserBookmarksResponseDto } from "@blue0206/members-only-shared-types";
+
+export default function bookmarksFilter(
+  data: GetUserBookmarksResponseDto,
+  searchQuery: string
+): GetUserBookmarksResponseDto {
+  return data.filter((bookmark) => {
+    const messageMatch = bookmark.message
+      .toLowerCase()
+      .includes(searchQuery.toLowerCase());
+    let authorMatch = false;
+    if (bookmark.user) {
+      authorMatch =
+        bookmark.user.username
+          .toLowerCase()
+          .includes(searchQuery.toLowerCase()) ||
+        bookmark.user.firstname
+          .toLowerCase()
+          .includes(searchQuery.toLowerCase()) ||
+        (bookmark.user.middlename ?? "")
+          .toLowerCase()
+          .includes(searchQuery.toLowerCase()) ||
+        (bookmark.user.lastname ?? "")
+          .toLowerCase()
+          .includes(searchQuery.toLowerCase());
+    }
+
+    return messageMatch || authorMatch;
+  });
+}
